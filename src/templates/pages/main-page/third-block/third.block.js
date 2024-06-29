af0d29f876c9236e90668791c83a4b26bc9e20a2
zaymovskey <zaymovskey@mail.com>
2024-06-29 20:50:09 +0300

    third block and swiper slider

---
+++ src/templates/pages/main-page/third-block/third.block.js
@@ -0,0 +1,7 @@
+new Swiper('.clients-slider', {
+    navigation: {
+        nextEl: '.clients-slider__next-button',
+        prevEl: '.clients-slider__prev-button',
+    },
+    slidesPerView: 5,
+})
