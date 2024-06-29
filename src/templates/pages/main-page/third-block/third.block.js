cdd7ba8fe703ffe5abdb4f90392cafec95fd068f
zaymovskey <zaymovskey@mail.com>
2024-06-29 21:41:59 +0300

    third block

--- src/templates/pages/main-page/third-block/third.block.js
+++ src/templates/pages/main-page/third-block/third.block.js
@@ -5,3 +5,12 @@ new Swiper('.clients-slider', {
     },
     slidesPerView: 5,
 })
+
+new Swiper('.reviews-slider', {
+    navigation: {
+        nextEl: '.reviews-slider__next-button',
+        prevEl: '.reviews-slider__prev-button',
+    },
+    slidesPerView: 3,
+    spaceBetween: 24,
+})
